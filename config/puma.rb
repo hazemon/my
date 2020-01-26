6a219bf993f6cf2978dac591e049439dd3345609
hazemon <hazesho1126@icloud.com>
2020-01-26 16:20:04 +0900

    Add

--- config/puma.rb
+++ config/puma.rb
@@ -34,3 +34,13 @@ on_worker_boot do
 end
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+preload_app!
+rackup DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
